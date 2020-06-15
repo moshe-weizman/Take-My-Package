f90877fe45a213d20ef14cee1a3a6a07ba12345c
ronip <ronipakter@gmail.com>
2020-06-15 22:18:18 +0300

    חבילת registeredParcels פרגמנט וXML וכן adapter לrecyclerView

--- app/src/main/java/com/example/takemypackage/Data/RegisteredParcels/RegisteredParcelsDataSource.java
+++ app/src/main/java/com/example/takemypackage/Data/RegisteredParcels/RegisteredParcelsDataSource.java
@@ -2,9 +2,11 @@ package com.example.takemypackage.Data.RegisteredParcels;
 
 import com.example.takemypackage.Entities.Parcel;
 
+import java.util.List;
+
 public class RegisteredParcelsDataSource {
-   public Parcel[] getRegisteredParcels(int memberId){
-return null;
+   public List<Parcel> getRegisteredParcels(){
+
    }
 
 }
