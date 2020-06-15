3ff4f04ba10a9277643f79f95160fe5b009c5b55
moshew1991 <moshew1991@gmail.com>
2020-06-15 23:28:33 +0300

    Pending Parcel class start

--- app/src/main/java/com/example/takemypackage/Entities/PendingParcel.java
+++ app/src/main/java/com/example/takemypackage/Entities/PendingParcel.java
@@ -1,6 +1,7 @@
 package com.example.takemypackage.Entities;
 
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Dictionary;
 import java.util.List;
@@ -10,6 +11,12 @@ public class PendingParcel extends Parcel {
       private String name;
       private String phone;
       private boolean isAuthorized;
+
+      public Deliverer(Member deliverer){
+         name = deliverer.getfName() + " " + deliverer.getlName();
+         phone = deliverer.getPhone();
+         isAuthorized = false;
+      }
    }
    private List<Deliverer> optionalDeliverers;
 
@@ -17,5 +24,19 @@ public class PendingParcel extends Parcel {
                         String recipientAddress, ParcelType type, boolean fragile, ParcelWeight weight,
                         String parcelID, String locationOfStorage, Status status) {
       super(recipientPhone, recipientFirstName, recipientLastName, recipientAddress, type, fragile, weight, parcelID, locationOfStorage, status);
+      optionalDeliverers = new ArrayList<Deliverer>();
+   }
+
+   public List<Deliverer> getOptionalDeliverers() {
+      return optionalDeliverers;
+   }
+
+   public void addOptionalDeliverer(Member deliverer) {
+      this.optionalDeliverers.add(new Deliverer(deliverer));
+   }
+   //TODO boom
+   public void authorize(String chosenManPhone){
+      optionalDeliverers.get(4434).isAuthorized = true;
+
    }
 }
