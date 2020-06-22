a11738db4119b1a66b748817277495064c9608c8
ronip <ronipakter@gmail.com>
2020-06-22 17:17:01 +0300

    חירבוש מחורבשש

--- app/src/main/java/com/example/takemypackage/UI/MainActivity/RegisteredParcelsFragment/RegisteredParcelsFragment.java
+++ app/src/main/java/com/example/takemypackage/UI/MainActivity/RegisteredParcelsFragment/RegisteredParcelsFragment.java
@@ -1,9 +1,11 @@
 package com.example.takemypackage.UI.MainActivity.RegisteredParcelsFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,35 +13,55 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.takemypackage.Entities.Parcel;
+import com.example.takemypackage.Data.PendingParcelsFirebaseManager;
+import com.example.takemypackage.Entities.Member;
 import com.example.takemypackage.Entities.PendingParcel;
 import com.example.takemypackage.R;
+import static com.example.takemypackage.UI.Login.LoginActivity.LoginActivity.MEMBER_KEY;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RegisteredParcelsFragment extends Fragment {
    private RecyclerView _recyclerView;
+   List<PendingParcel> registeredParcels;
+   private Member member;
 
    @Nullable
    @Override
    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.registered_parcels_fragment, container, false);
-      RecyclerView recyclerView = view.findViewById(R.id.registeredParcelsRecyclerView);
+      final RecyclerView recyclerView = view.findViewById(R.id.registeredParcelsRecyclerView);
       recyclerView.setHasFixedSize(true);
       RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this.getContext());
       recyclerView.setLayoutManager(layoutManager);
 
-      List<PendingParcel> registeredParcels = new ArrayList<>();
-      RecyclerView.Adapter adapter = new RegisteredParcelsRecyclerViewAdapter(registeredParcels);
-      recyclerView.setAdapter(adapter);
+      Intent intent = this.getActivity().getIntent();
+      member = (Member)intent.getSerializableExtra(MEMBER_KEY);
 
+      PendingParcelsFirebaseManager.NotifyToParcelList(new PendingParcelsFirebaseManager.NotifyDataChange<List<PendingParcel>>() {
+         @Override
+         public void OnDataChanged(List<PendingParcel> obj) {
+            if (recyclerView.getAdapter() == null){
+               for (PendingParcel pendingParcel : obj) {
+                  if (pendingParcel.getParcelDetails().getRecipientPhone().equals(member.getPhone())){
+                     registeredParcels.add(pendingParcel);
+                  }
+               }
+               recyclerView.setAdapter(new RegisteredParcelsRecyclerViewAdapter(registeredParcels, member));
+            }else recyclerView.getAdapter().notifyDataSetChanged();
+         }
+
+         @Override
+         public void onFailure(Exception exception) {
+            Toast.makeText(getContext(), "Failed to get your registered parcels data \n" + exception.toString(), Toast.LENGTH_LONG).show();
+         }
+      });
       return view;
    }
 
    @Override
    public void onCreate(@Nullable Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-
    }
 }
--- app/src/main/java/com/example/takemypackage/UI/MainActivity/RegisteredParcelsFragment/RegisteredParcelsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/takemypackage/UI/MainActivity/RegisteredParcelsFragment/RegisteredParcelsRecyclerViewAdapter.java
@@ -3,11 +3,15 @@ package com.example.takemypackage.UI.MainActivity.RegisteredParcelsFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.takemypackage.Entities.Member;
+import com.example.takemypackage.Entities.Parcel;
 import com.example.takemypackage.Entities.PendingParcel;
 import com.example.takemypackage.R;
 
@@ -15,39 +19,51 @@ import java.util.List;
 
 public class RegisteredParcelsRecyclerViewAdapter extends RecyclerView.Adapter<RegisteredParcelsRecyclerViewAdapter.RegisteredParcelsViewHolder> {
    private List<PendingParcel> _registeredParcels;
+   Member _member;
 
    public static class RegisteredParcelsViewHolder extends RecyclerView.ViewHolder {
       // each data item is just a string in this case
-      public TextView addressTextView;
-      public TextView weightTextView;
-      public TextView distanceFromMeTextView;
-      public TextView storageTimeTextView;
+      public TextView parcelIdTextView;
+      public TextView fragileTextView;
+      public TextView parcelTypeTextView;
+      public TextView parcelWeightTextView;
+      public TextView recipientAddressTextView;
+      public TextView storageLocation;
+      public Spinner deliverySpinner;
 
       public RegisteredParcelsViewHolder(@NonNull View itemView) {
          super(itemView);
-         addressTextView = (TextView) itemView.findViewById(R.id.serialNumTextView);
-         weightTextView = (TextView) itemView.findViewById(R.id.fregileTextView);
-         distanceFromMeTextView = (TextView) itemView.findViewById(R.id.parcelTypeTextView);
-         storageTimeTextView = (TextView) itemView.findViewById(R.id.parcelWeightTextView);
+         parcelIdTextView = itemView.findViewById(R.id.serialNumTextView);
+         fragileTextView = itemView.findViewById(R.id.fragileTextView);
+         parcelTypeTextView = itemView.findViewById(R.id.parcelTypeTextView);
+         parcelWeightTextView = itemView.findViewById(R.id.parcelWeightTextView);
+         recipientAddressTextView = itemView.findViewById(R.id.recipientAddressTextView);
+         storageLocation = itemView.findViewById(R.id.storageLocationTextView);
       }
    }
 
-   public RegisteredParcelsRecyclerViewAdapter(List<PendingParcel> registeredParcels) {
+   public RegisteredParcelsRecyclerViewAdapter(List<PendingParcel> registeredParcels, Member member) {
       _registeredParcels = registeredParcels;
+      _member = member;
    }
 
    @NonNull
    @Override
    public RegisteredParcelsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-      View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listed_parcel_itemview, parent, false);
+      View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.listed_registered_parcel_itemview, parent, false);
       return new RegisteredParcelsViewHolder(view);
    }
 
    @Override
    public void onBindViewHolder(@NonNull RegisteredParcelsViewHolder holder, int position) {
-      holder.addressTextView.setText(_registeredParcels.get(position).getParcelDetails().getRecipientAddress());
-      holder.weightTextView.setText(_registeredParcels.get(position).getParcelDetails().getWeight().toString());
-      //to do: inserting distance from me and storage time
+      PendingParcel pendingParcel = _registeredParcels.get(position);
+      Parcel parcelDetails = pendingParcel.getParcelDetails();
+      holder.parcelIdTextView.setText(parcelDetails.getParcelID());
+      holder.fragileTextView.setVisibility(parcelDetails.isFragile()? TextView.VISIBLE : TextView.INVISIBLE);
+      holder.storageLocation.setText(parcelDetails.getLocationOfStorage());
+      holder.recipientAddressTextView.setText(parcelDetails.getRecipientAddress());
+      holder.parcelTypeTextView.setText(parcelDetails.getType().toString());
+      holder.parcelWeightTextView.setText(parcelDetails.getWeight().toString());
    }
 
    @Override
